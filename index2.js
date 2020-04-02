582c023a8d27f3024a01287518a1d788dd93534e
Federico <cuervog992@gmail.com>
2020-04-02 20:15:40 -0300

    crear archivos
    primeros archivos

---
+++ index2.js
@@ -0,0 +1,30 @@
+function processData(data) {
+  data.data.map(function(e) {
+    const gif = e.images.original.url;
+    const img = document.createElement("IMG");
+    img.setAttribute("src", gif);
+    document.getElementById("contenedorGif").appendChild(img);
+  });
+}
+
+const buttonSearch = document.getElementById("boton-buscador");
+buttonSearch.addEventListener("click", function() {
+  document.getElementById("contenedorGif").innerHTML = "";
+  const inputValue = document.getElementById("buscador").value;
+
+  const apiKey = "cMm1tYzqFl0jlQ83RRs4q4MMVlT9kMDx";
+  const search = inputValue;
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=3&rating=G&lang=en`;
+
+  fetch(endpoint)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      processData(data);
+    })
+    .catch(function(error) {
+      return error;
+    });
+  document.getElementById("buscador").innerHTML = "";
+});
