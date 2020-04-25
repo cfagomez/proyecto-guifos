11c9fbde0242270f3b6b660481e26ee8a337c709
Federico <cuervog992@gmail.com>
2020-04-25 15:46:47 -0300

    funciones para dark theme

--- darkTheme.js
+++ darkTheme.js
@@ -2,4 +2,10 @@ const dark = document.getElementById("darkTheme");
 dark.addEventListener("click", function () {
   const principalTheme = document.getElementById("theme");
   principalTheme.setAttribute("href", "style2.css");
+  const occultDropdown = document.getElementById("occult-items-dropdown");
+  if (occultDropdown.style.display === "none") {
+    occultDropdown.style.display = "block";
+  } else {
+    occultDropdown.style.display = "none";
+  }
 });
