dcf9a339a3f920f802970f19144a0ac22ec169c9
Federico <cuervog992@gmail.com>
2020-06-08 23:20:28 -0300

    funciones para volver pag anterior, ir al inicio y seccion mis gifos

--- crearGuifosSect.js
+++ crearGuifosSect.js
@@ -1,8 +1,40 @@
-const crearGuifosButton = document.getElementById("crearGuifosButton");
-crearGuifosButton.addEventListener("click", function () {
-  document.getElementById("itemsContainer").style.visibility = "hidden";
-  document.getElementById("buscadorSectionContainer").style.display = "none";
-  document.getElementById("sugerenciasContainer").style.display = "none";
-  document.getElementById("tendenciasSectionContainer").style.display = "none";
-  document.getElementById("crearGuifosWindow").style.display = "flex";
+function procesarDatos(datos) {
+  datos.data.map(function (e) {
+    const gif = e.images.downsized.url;
+    const img = document.createElement("IMG");
+    img.setAttribute("src", gif);
+    img.setAttribute("width", "100%");
+    img.setAttribute("height", "250px");
+    document.getElementById("misGifosGifsContainer").appendChild(img);
+  });
+}
+
+fetch(
+  "https://api.giphy.com/v1/gifs/search?api_key=cMm1tYzqFl0jlQ83RRs4q4MMVlT9kMDx&q=80s horror movies&limit=8&offset=0&rating=R&lang="
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (datos) {
+    console.log(datos);
+    procesarDatos(datos);
+  })
+  .catch(function (error) {
+    return error;
+  });
+
+const backArrow = document.getElementById("backArrow");
+backArrow.addEventListener("click", function () {
+  window.history.back();
 });
+
+const cancelarButton = document.getElementById("cancelarButton");
+cancelarButton.addEventListener("click", function () {
+  document.getElementById("crearGuifosWindow").style.display = "none";
+});
+
+document
+  .getElementById("comenzarButton")
+  .addEventListener("click", function () {
+    document.getElementById("wrapperMisGifosSection").style.display = "none";
+  });
